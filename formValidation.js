bbc9b35573bba23b6c147e7e710746eea667d578
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-11-29 19:00:44 +0000

    code cleaning

--- formValidation.js
+++ formValidation.js
@@ -1,18 +1,3 @@
-/* function ValidateEmail(emailInput, nameInput, occInput) {
-    const mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
-    if(!emailInput.value.match(mailformat)) {
-        // alert("You have entered an invalid email address!");
-        document.getElementById("email-err").style.visibility="visible";
-        // document.getElementById("email").focus();
-        // return false;
-    
-    } else if(emailInput.value.match(mailformat) && nameInput.length > 0 && occInput.length > 0){
-        alert("Valid email address!");
-        // return true;
-    }
-} */
-
 const email = document.getElementById('MERGE0');
 const name = document.getElementById('MERGE1');
 const occupation = document.getElementById('MERGE2');
